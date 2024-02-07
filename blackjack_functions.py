6f9e958aad6a20cf3bf7f9f73feb9772caa00d8e
ManavBijlani <119971901+ManavBijlani@users.noreply.github.com>
2024-02-07 15:26:57 +1030

    Defined a take_bet function This function takes the bet of a player and checks if it's valid

---
+++ blackjack_functions.py
@@ -0,0 +1,28 @@
+import blackjack_classes
+
+#Importing the shuffle function from the random module to shuffle the deck afterwards
+from random import shuffle 
+#Initializing lists for cards
+suits=['clubs','hearts','diamonds','spades']
+ranks=['two','three','four','five','six','seven','eight','nine','ten','jack','queen','king','ace']
+values={'two':2,'three':3,'four':4,'five':5,'six':6,'seven':7,'eight':8,'nine':9,'ten':10,'jack':11,'queen':11,
+       'king':11,'ace':11}
+
+
+#Function for taking the bet from the player
+def take_bet(player_chips):
+    while True:
+        try:
+            bet=int(input("Enter the bet to be made by the player:")) 
+        except:
+            print("Please enter an integer value")
+            continue
+        else:
+            if bet>player_chips.total_chips:
+                print('The bet should not exceed the total chips you have!')
+                continue
+            else:
+                break
+        finally:
+            print('Thank you!')
+    return bet
